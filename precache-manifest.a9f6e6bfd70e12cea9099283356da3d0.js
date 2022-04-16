9a7d469952cb9d84257a8f147393d75f9385ff7a
nitesh <itskynitesh@gmail.com>
2022-04-16 13:14:16 +0530

    Updates

---
+++ precache-manifest.a9f6e6bfd70e12cea9099283356da3d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "37c8dc4b07e89e951841",
+    "url": "/myportfolio-/static/css/main.77f6f47a.chunk.css"
+  },
+  {
+    "revision": "37c8dc4b07e89e951841",
+    "url": "/myportfolio-/static/js/main.37c8dc4b.chunk.js"
+  },
+  {
+    "revision": "353df1f1b8110f645f80",
+    "url": "/myportfolio-/static/js/1.353df1f1.chunk.js"
+  },
+  {
+    "revision": "1c9e80ff9835afc82c40",
+    "url": "/myportfolio-/static/js/runtime~main.1c9e80ff.js"
+  },
+  {
+    "revision": "84cfe78fc371a005661bfa574a3f0e69",
+    "url": "/myportfolio-/static/media/Niteshkumar1.84cfe78f.pdf"
+  },
+  {
+    "revision": "aa65660550634ddadaf83d84e58d1168",
+    "url": "/myportfolio-/static/media/img2.aa656605.jpeg"
+  },
+  {
+    "revision": "4325eb0f34548b9f7cc425c36f268f96",
+    "url": "/myportfolio-/static/media/bg.4325eb0f.jpeg"
+  },
+  {
+    "revision": "b58d8fabab9c90a6ee447726156a1d4f",
+    "url": "/myportfolio-/static/media/profile.b58d8fab.jpeg"
+  },
+  {
+    "revision": "f4324a55ec06ae16c38dbfd3c49ae65c",
+    "url": "/myportfolio-/index.html"
+  }
+];
